c6c9af1455c5ae1c4bc10d5386044dd045851ac1
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-05-13 23:20:35 -0300

    adicionado algumas validações de entrada

--- calculadora.py
+++ calculadora.py
@@ -17,8 +17,15 @@ while menu != 3:
     if menu == 1:
         os.system('clear')
 
-        nome = str(input('Digite o nome do cliente: '))
-        data = str(input('Digite a data da transação: '))
+        cliente = str(input('Digite o nome do Cliente: '))
+        while len(cliente) <=2:
+            os.system('clear')
+            nome = str(input('Você precisa digitar o nome do seu Cliente: '))
+
+        data = str(input('Digite a Data da Transação: '))
+        while len(data) < 10:
+            os.system('clear')
+            data = str(input('Você precisa digitar uma Data para a Transação (ex: 00/00/0000): '))
 
         os.system('clear')
 
@@ -34,18 +41,33 @@ while menu != 3:
             nomeMoeda = {1: 'REAL', 2: 'DOLAR', 3: 'EURO', 4: 'PESO ARG'}
 
             moedaOrigem = int(input('Escolha uma Moeda de Origem disponível no menu: '))
-            numero1 = float(input('Digite o valor da Moeda de Origem: '))
+            while (moedaOrigem < 1) or (moedaOrigem > 4):
+                moedaOrigem = int(input('OPÇÃO INVÁLIDA - Escolha uma moeda apresentada no menu: '))
+
+            numero1 = float(input('Digite o valor em {}: '.format(nomeMoeda[moedaOrigem])))
+            while numero1 < 1:    
+                numero1 = float(input('Você deve digitar um valor em {} para prosseguir: '.format(nomeMoeda[moedaOrigem])))
             print('----------------------------------')
+
             moedaDestino = int(input('Escolha uma Moeda de Destino disponível no menu: '))
-            if moedaOrigem == moedaDestino:
-                print('A Moeda de Destino deve ser DIFERENTE da Moeda de Origem!')
+            while (moedaDestino < 1) or (moedaDestino > 4):
+                moedaDestino = int(input('OPÇÃO INVÁLIDA - Escolha uma moeda apresentada no menu: '))
+            
+            while moedaOrigem == moedaDestino:
+                moedaDestino = int(input('A Moeda de Destino deve ser DIFERENTE da Moeda de Origem, escolha novamente: '))
+
             numero2 = float(input('Digite o valor da cotação do {} em relação ao {} hoje: '.format(nomeMoeda[moedaDestino], nomeMoeda[moedaOrigem])))
+            while numero2 < 1:    
+                numero2 = float(input('Você deve digitar o valor da cotação do {} para prosseguir: '.format(nomeMoeda[moedaDestino])))
             print('----------------------------------')
 
             siglaMoeda = {1: 'BRL', 2: 'US$', 3: 'EUR', 4: 'ARS'}
 
             resultado = numero1 / numero2
             resultadoTaxa = resultado / 10
+            print('Transação Conlcuida com Sucesso!!!')
+            print()
             print('Valor da conversão: {} {:.2f}'.format(siglaMoeda[moedaDestino], resultado))
             print('Valor dos 10% da Taxa de Transação: R$ {:.2f}'.format(resultadoTaxa))
+            print('----------------------------------')
             input()
